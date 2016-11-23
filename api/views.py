c1c8b5ee86f6cdd4f1435eb872bfc81286058512
tetriseffect <avakyan@gmail.com>
2016-11-23 06:45:35 -0800

    Able to modify PDF fields in mail

--- api/views.py
+++ api/views.py
@@ -9,133 +9,12 @@ from api.serializers import MailItemSerializer
 
 import lob
 
+
 # Test Key
 lob.api_key = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'
 
-
-html_file = """
-<html>
-<head>
-<meta charset="UTF-8">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-<title>Lob.com Sample Letter</title>
-<style>
-  *, *:before, *:after {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-
-  body {
-    width: 8.5in;
-    height: 11in;
-    margin: 0;
-    padding: 0;
-  }
-
-  .page {
-    page-break-after: always;
-  }
-
-  .page-content {
-    position: relative;
-    width: 8.125in;
-    height: 10.625in;
-    left: 0.1875in;
-    top: 0.1875in;
-    background-color: rgba(0,0,0,0.2);
-  }
-
-  .text {
-    position: relative;
-    left: 20px;
-    top: 20px;
-    width: 6in;
-    font-family: 'Open Sans';
-    font-size: 30px;
-  }
-
-  #return-address-window {
-    position: absolute;
-    left: .625in;
-    top: .5in;
-    width: 3.25in;
-    height: .875in;
-    background-color: rgba(255,0,0,0.5);
-  }
-
-  #return-address-text {
-    position: absolute;
-    left: .07in;
-    top: .34in;
-    width: 2.05in;
-    height: .44in;
-    background-color: white;
-    font-size: .11in;
-  }
-
-  #return-logo {
-    position: absolute;
-    left: .07in;
-    top: .02in;
-    width: 2.05in;
-    height: .3in;
-    background-color: white;
-  }
-
-  #recipient-address-window {
-    position: absolute;
-    left: .625in;
-    top: 1.75in;
-    width: 4in;
-    height: 1in;
-    background-color: rgba(255,0,0,0.5);
-  }
-
-  #recipient-address-text {
-    position: absolute;
-    left: .07in;
-    top: .05in;
-    width: 2.92in;
-    height: .9in;
-    background-color: white;
-  }
-
-</style>
-</head>
-
-<body>
-  <div class="page">
-    <div class="page-content">
-      <div class="text" style="top: 3in">
-        The grey box is the safe area. Do not put text outside this box. If you are using the data argument, you can add variables like this: {{variable_name}}.
-      </div>
-    </div>
-    <div id="return-address-window">
-      <div id="return-logo">
-        Room for company logo.
-      </div>
-      <div id="return-address-text">
-        The Return Address will be printed here. The red area will be visible through the envelope window.
-      </div>
-    </div>
-    <div id="recipient-address-window">
-      <div id="recipient-address-text">
-        The Recipient's Address will be printed here. The red area will be visible through the envelope window.
-      </div>
-    </div>
-  </div>
-  <div class="page">
-    <div class="page-content">
-      <div class="text">
-        This is a second page.
-      </div>
-    </div>
-  </div>
-</body>
-
-</html>
-"""
+# Temporary message template 
+template_url = 'https://github.com/lob/lob-templates/raw/master/letters/outstanding-balance/outstanding-balance.pdf'
 
 
 @api_view(['GET', 'POST'])
@@ -154,9 +33,11 @@ def mail_item_list(request):
         if serializer.is_valid():
             serializer.save()
 
+            mailItem = serializer.validated_data
+
             letter = lob.Letter.create(
                 to_address = {
-                    'name': 'Harry Zhang',
+                    'name': mailItem['message'],
                     'address_line1': '123 Test Street',
                     'address_city': 'Mountain View',
                     'address_state': 'CA',
@@ -171,7 +52,7 @@ def mail_item_list(request):
                     'address_zip': '94041',
                     'address_country': 'US'
                 },
-                file = html_file,
+                file = template_url,
                 data = {
                     'name': 'Harry'
                 },
